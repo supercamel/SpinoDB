fc51354e79cf0798898b4ebed9419f08d4919e91
Sam Cowen <samuel.cowen@camelsoftware.com>
2022-03-06 20:39:29 +1100

    added C example

---
+++ examples/c/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Spino-1.0.h>
+
+int main() {
+    SpinoDatabase* db = spino_database_new();
+
+    SpinoCollection* col = spino_database_add_collection(db, "my_collection");
+    spino_collection_create_index(col, "idx");
+
+    for(int i = 0; i < 10; i++) {
+        GString* s = g_string_new(NULL);
+        g_string_printf(s, "{\"idx\": %i}", i);
+
+        spino_collection_append(col, s->str);
+
+        g_string_free(s, TRUE);
+    }
+    
+    SpinoCursor* cur = spino_collection_find(col, "{}");
+    while(spino_cursor_has_next(cur) == TRUE) {
+        printf("%s\n", spino_cursor_next(cur));
+    }
+
+    spino_database_save(db, "db.json");
+    return 0;
+}
+
